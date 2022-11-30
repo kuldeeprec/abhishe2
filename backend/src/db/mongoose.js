a415bc0055366563ee26cb6af4ba35d79df6a99e
Kuldeep Yadav <kuldeepy10459@gmail.com>
2022-12-01 00:42:37 +0530

    added

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI,{
+console.log(process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex : true
-}).then(()=>{
-    console.log("Connection Successful")
-}).catch((err)=>{
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((err) => {
     console.log("Unsuccessful");
     //console.log("err");
-})
+  });
